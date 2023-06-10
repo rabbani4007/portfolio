6afb3bfb0fa8e3b6a5faf4bd69178f461f5f599d
Ghulam Rabbani <rabbani4007@gmail.com>
2023-06-11 01:32:14 +0530

    finish the code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-// scroll section active link
+/*================================= toggle icon navbar =================================*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/*================================= scroll section active link =================================*/
 
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
@@ -19,4 +28,38 @@ window.onscroll = () => {
       });
     }
   });
+  /*================================= sticky navbar =================================*/
+
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /*================================= remove toggle icon and  navbar  when click (scroll link) =================================*/
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
+
+/*================================= scroll reveal =================================*/
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  deley: 200,
+});
+
+ScrollReveal().reveal(".home-content,.heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img,.services-container,.portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1,.about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p,.about-content", { origin: "right" });
+
+/*================================= typed js =================================*/
+const typed = new Typed(".multiple-text", {
+  strings: ["FULL STACK DEVELOPER", "ML ENTHUSIAST", "PHOTOGRAPHER"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
